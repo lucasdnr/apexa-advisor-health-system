8794916bb868878266f897b3a95fde0057cc980d
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-04-17 20:43:10 -0400

    add routes class

--- AdvisorHealthAPI/AdvisorHealthAPI/Program.cs
+++ AdvisorHealthAPI/AdvisorHealthAPI/Program.cs
@@ -1,3 +1,5 @@
+using AdvisorHealthAPI.Routes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,23 +16,9 @@ if(app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Routes
+AdvisorsRoutes.AddRoutesAdvisors(app);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
 
 app.Run();
 
---
+++ AdvisorHealthAPI/AdvisorHealthAPI/Routes/AdvisorsRoutes.cs
@@ -0,0 +1,9 @@
+﻿namespace AdvisorHealthAPI.Routes;
+
+public static class AdvisorsRoutes
+{
+    public static void AddRoutesAdvisors(WebApplication app)
+    {
+        app.MapGet("advisors", () => "hello world");
+    }
+}
