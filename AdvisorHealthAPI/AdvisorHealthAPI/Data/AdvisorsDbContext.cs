13cfe7f08537ccef86492eedddc2fee6637aab4b
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-04-17 21:21:49 -0400

    Add db context for advisors model

---
+++ AdvisorHealthAPI/AdvisorHealthAPI/Data/AdvisorsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AdvisorHealthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvisorHealthAPI.Data;
+
+public class AdvisorsDbContext : DbContext
+{
+    private DbSet<Advisor> Advisors {  get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("AdvisorsDb");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
